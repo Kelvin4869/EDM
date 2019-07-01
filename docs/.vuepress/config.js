9ff6e988118ca2b87bcec9309994adba6914aa21
Kelvin4869 <kuangsixiang@163.com>
2019-07-01 15:59:47 +0800

    .

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,31 +1,18 @@
 module.exports = {
-  title: '网站标题',
-  description: '网站描述',
+  title: "Kelvin's notebook",
+  description: 'Go big or go home!!!',
   // 注入到当前页面的 HTML <head> 中的标签
   head: [
     ['link', { rel: 'icon', href: '/logo.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
-  base: '/web_accumulate/', // 这是部署到github相关的配置 下面会讲
+  base: '/EDM/', // github仓库相关的配置
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
   themeConfig: {
+    nav: require('./nav'),
+    sidebar: require('./sidebar'),
     sidebarDepth: 2,
     lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
-    nav:[
-      { text: '前端算法', link: '/algorithm/' }, // 内部链接 以docs为根目录
-      { text: '博客', link: 'http://obkoro1.com/' }, // 外部链接
-      // 下拉列表
-      {
-        text: 'GitHub',
-        items: [
-          { text: 'GitHub地址', link: 'https://github.com/OBKoro1' },
-          {
-            text: '算法仓库',
-            link: 'https://github.com/OBKoro1/Brush_algorithm'
-          }
-        ]
-      }        
-    ]
   }
 };
---
+++ docs/.vuepress/nav.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { text: '笔记本', link: '/accumulate/' }, // 内部链接 以docs为根目录
+  { text: '前端网站导航', link: 'https://www.frontendnav.top/' }, // 前端网站导航
+  { text: 'GitHub', link: 'https://github.com/Kelvin4869' }, // GitHub
+];
