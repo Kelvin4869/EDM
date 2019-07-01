e2809af542976a1ab9cdcc96fecb827fa68c275a
Kelvin4869 <kuangsixiang@163.com>
2019-07-01 16:30:06 +0800

    .

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/logo.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
-  base: '/EDM/', // github仓库相关的配置
+  base: '/EdmPublicDist/', // github仓库相关的配置
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
